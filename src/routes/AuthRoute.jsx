bb27dd52ca2b04c5466c476466a31452dca610bb
gyeonggumun <lion989072@gmail.com>
2025-12-17 12:11:59 +0900

    잘 못된 경로 이동 막는 로직

--- src/routes/AuthRoute.jsx
+++ src/routes/AuthRoute.jsx
@@ -3,18 +3,33 @@ import Login from "../pages/auth/Login";
 import SignUp from "../pages/auth/SignUp";
 import { useEffect } from "react";
 import OAuth2 from "../pages/auth/OAuth2";
+import { useMeQuery } from "../queries/usersQueries";
 
 function AuthRoute() {
     const navigate = useNavigate();
     const location = useLocation();
     const {pathname} = location;
 
-    useEffect(() => {
-        if (pathname === "/") {
-            navigate("/auth/login");
-        }
-    }, [pathname]);
+    const meQuery = useMeQuery();
 
+    useEffect(() => {
+        const { isLoading, data } = meQuery;
+        // 잘 못된 경로로 들어온 사람을 올바른 경로로 이동시켜주는 로직
+        if (!isLoading) {
+            if (data.status !== 200) {
+                // 로그인 버튼을 눌렀을 때 로그인이 되어 있지 않은 상태라면 로그인 페이지로 이동시켜줌
+                if (!["/auth/login", "/auth/login/oauth2"].includes(pathname)) {
+                    navigate("/auth/login");
+                }
+            } else {
+                // 로그인이 되어있는 상태인데 로그인 페이지로 넘어가려 하면 홈화면으로 이동시켜줌
+                if (["/auth/login", "/auth/login/oauth2"].includes(pathname)) {
+                    navigate("/");
+                }
+            }
+        }
+    }, [pathname, meQuery.data]);
+    
     return <Routes>
         <Route path="/" element={<></>} />
         <Route path="/auth/login" element={<Login />} />
