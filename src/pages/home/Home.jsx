fcb9867eeb8a9ccf2e2702a2ca17ab16962bf64a
gyeonggumun <lion989072@gmail.com>
2025-12-22 12:21:55 +0900

    home 화면 세팅 중간업로드

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import  * as s  from "./styles";
 
 function Home() {
-    return <div>
-
+    return <div css={s.layout}>
+        <div css={s.feedContainer} >
+            <div css={s.feedItemContainer}>
+                <header>
+                    <div css={s.profileImage()}></div>
+                    <div css={s.userInfo}>
+                        <div>닉네임</div>
+                        <div>작성일자</div>
+                    </div>
+                </header>
+                <main></main>
+                <footer></footer>
+            </div>
+        </div>
+        <div css={s.followInfoContainer} ></div>
     </div>
 }
 
