9f8cd0a93646cb3ba11d107265068777622b6cba
gyeonggumun <lion989072@gmail.com>
2026-01-02 19:37:01 +0900

    Chatscroll 로직변경

--- src/components/openai/OpenaiApiModal.jsx
+++ src/components/openai/OpenaiApiModal.jsx
@@ -1,18 +1,23 @@
 /** @jsxImportSource @emotion/react */
-import  * as s  from "./styles";
-
+import * as s from "./styles";
 import { useEffect, useRef, useState } from "react";
 import { sendTextOpenai } from "../../apis/openai/openaiApi";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import 'highlight.js/styles/github-dark.css';
-import {PulseLoader} from "react-spinners"
+import {PulseLoader} from "react-spinners";
 import { MdUpload } from "react-icons/md";
 
-function TypingEffect({text, speed = 50}) {
+function TypingEffect({text, speed = 50, chatScroll}) {
     const [ displayText, setDisplayText ] = useState("");
     const textIndex = useRef(0);
 
+    useEffect(() => {
+        chatScroll.current?.scrollTo({
+            top: chatScroll.current.scrollHeight,
+            behavior: "smooth"
+        });
+    }, [displayText])
 
     useEffect(() => {
         const textArray = Array.from(text);
@@ -36,15 +41,16 @@ function TypingEffect({text, speed = 50}) {
 }
 
 function OpenaiApiModal() {
-    const [ chatData, setChatData] = useState([]);
+    const [ chatData, setChatData ] = useState([]);
     const [ inputValue, setInputValue ] = useState("");
     const [ isLoading, setLoading ] = useState(false);
     const [ disabled, setDisabled ] = useState(true);
+    const chatScroll = useRef();
 
     const handleOnKeyDown = (e) => {
         if(!e.shiftKey && e.key === "Enter") {
-            e.preventDefault();  // 기본동작 막는 행위
-
+            e.preventDefault();
+    
             if(inputValue.trim()) {
                 handleSend();
             }
@@ -54,7 +60,7 @@ function OpenaiApiModal() {
     const handleSend = () => {
         setLoading(true);
         setChatData(prev => [...prev, {
-            type:  "question",
+            type: "question",
             content: inputValue,
         }]);
         setInputValue("");
@@ -65,10 +71,10 @@ function OpenaiApiModal() {
     }, [inputValue]);
 
     useEffect(() => {
-        if (!!chatData.length && chatData[chatData.length - 1].type == "question"){
+        if (!!chatData.length && chatData[chatData.length - 1].type === "question") {
             sendTextOpenai(chatData[chatData.length - 1].content)
             .then(resp => {
-                setChatData(prev => [...prev,  {
+                setChatData(prev => [...prev, {
                     type: "answer",
                     content: resp.output_text,
                 }]);
@@ -76,35 +82,43 @@ function OpenaiApiModal() {
         } else {
             setLoading(false);
         }
+        
     }, [chatData]);
+    
+    useEffect(() => {
+        chatScroll.current?.scrollTo({
+            top: chatScroll.current.scrollHeight,
+            behavior: "smooth"
+        });
+    }, [chatData, isLoading])
 
     return <div css={s.layout}>
-        <div css={s.chatContainer}> 
+        <div css={s.chatContainer} ref={chatScroll}>
             {
                 chatData.map((data, index) => {
                     if (data.type === "question") {
                         return <div key={index} css={s.question}>{data.content}</div>
                     } else if (index === chatData.length - 1) {
                         return <div key={index} css={s.answer}>
-                            {                            
-                                !isLoading && !!data.content && 
-                                <TypingEffect text={data.content} speed={10}/>
+                            {
+                                !isLoading && !!data.content &&
+                                <TypingEffect text={data.content} speed={10} chatScroll={chatScroll} />
                             }
                         </div>
                     } else {
                         return <div key={index} css={s.answer}>
                             <ReactMarkdown rehypePlugins={rehypeHighlight}>{data.content}</ReactMarkdown>
-                            </div>
+                        </div>
                     }
                 })
             }
             {
-                isLoading && <PulseLoader/>
+                isLoading && <PulseLoader />
             }
         </div>
-        <div css={s.inputContainer}> 
+        <div css={s.inputContainer}>
             <textarea value={inputValue} onKeyDown={handleOnKeyDown} onChange={(e) => setInputValue(e.target.value)} />
-            <button onClick={handleSend} disabled={disabled}><MdUpload/></button>
+            <button onClick={handleSend} disabled={disabled}><MdUpload /></button>
         </div>
     </div>
 }
